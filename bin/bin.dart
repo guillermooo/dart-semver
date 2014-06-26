395659022e9f1eb2c6158e70e7db1417b8b7a0c2
guillermooo <guillermo.lopez@outlook.com>
2014-06-26 21:40:04 +0200

    add main code

---
+++ bin/bin.dart
@@ -0,0 +1,5 @@
+import 'package:semver/semver.dart';
+
+void main() {
+
+}
---
+++ lib/semver.dart
@@ -0,0 +1,125 @@
+library semver;
+
+import 'package:semver/source/tokenizer.dart' show SemanticVersionTokenizer;
+
+class SemanticVersion {
+  /*
+  * Represents a tag in the semantic version format: http://semver.org/.
+  */
+  final int major;
+  final int minor;
+  final int patch;
+  final String pre;
+  final String build;
+
+  const SemanticVersion(this.major, this.minor, this.patch, [this.pre,
+      this.build]);
+
+  factory SemanticVersion.fromMap(Map tag) {
+    return new SemanticVersion(tag['major'], tag['minor'], tag['patch'],
+      tag['pre'], tag['build']);
+  }
+
+  factory SemanticVersion.fromString(String tag) {
+    SemanticVersionTokenizer t = new SemanticVersionTokenizer(tag);
+    return new SemanticVersion.fromMap(t.tokenize());
+  }
+
+  operator ==(SemanticVersion other) {
+    return (this.major == other.major && this.minor == other.minor &&
+        this.patch == other.patch && this.pre == other.pre &&
+        this.build == other.build);
+  }
+
+  operator <(SemanticVersion other) {
+    if (this.major < other.major) {
+      return true;
+    }
+
+    if (this.major == other.major) {
+      if (this.minor < other.minor) {
+        return true;
+      } else if (this.minor == other.minor) {
+        if (this.patch != other.patch && this.patch < other.patch) {
+          return true;
+        } else if (this.patch != other.patch) {
+          return false;
+        }
+      }
+    }
+
+    print(
+      "WARNING: comparing '{$this.pre}' and '${other.pre}' as strings!");
+
+    if (this.pre.isEmpty && other.pre.isEmpty &&
+        this.build.isEmpty && other.build.isEmpty) {
+      return false;
+    } else if (this.pre.isEmpty && other.pre.isNotEmpty) {
+      return true;
+    } else if (this.pre.isNotEmpty && other.pre.isEmpty) {
+      return false;
+    }
+
+    print(
+      "WARNING: comparing '{$this.build}' and '${other.build}' as strings!");
+
+    if (this.build.isEmpty && other.build.isEmpty) {
+      return false;
+    } else if (this.build.isEmpty && other.build.isNotEmpty) {
+      return true;
+    } else if (this.build.isEmpty && other.build.isNotEmpty) {
+      return false;
+    }
+
+    var sorted = new List<String>();
+
+    if (this.pre.isNotEmpty && other.pre.isNotEmpty) {
+      if (this.pre == other.pre) {
+        if (this.build == other.build) {
+          return false;
+        }
+        var sorted = [this.build, other.build]..sort();
+        return identical(sorted[0], this.build);
+      }
+    }
+
+    sorted = [this.pre, other.pre]..sort();
+    return identical(sorted[0], this.pre);
+  }
+
+  operator >(SemanticVersion other) {
+    if (this == other || this < other) {
+      return false;
+    }
+    return true;
+  }
+
+  operator >=(SemanticVersion other) {
+    if (this > other || this == other ) {
+      return true;
+    }
+    return false;
+  }
+
+  operator <=(SemanticVersion other) {
+    if (this < other || this == other) {
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  String toString() {
+    var version = "${this.major}.${this.minor}.${this.patch}";
+
+    if (this.pre != null) {
+        version = "$version-${this.pre}";
+    }
+
+    if (this.build != null) {
+        version = "$version+${this.build}";
+    }
+
+    return version;
+  }
+}
