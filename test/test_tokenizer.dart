a8936d9f0a4d31aef7682bdfe45e112885d4c818
guillermooo <guillermo.lopez@outlook.com>
2014-06-28 08:24:17 +0200

    refactor tokenizer tests

--- test/test_tokenizer.dart
+++ test/test_tokenizer.dart
@@ -2,104 +2,72 @@ import 'package:unittest/unittest.dart';
 import 'package:semver/source/tokenizer.dart';
 
 void main() {
-  test('can tokenize simple tag', () {
-    var tag = '0.1.0';
-    var sm = new SemanticVersionTokenizer(tag);
-    var expected = new Map();
-    expected['major'] = 0;
-    expected['minor'] = 1;
-    expected['patch'] = 0;
-    expect(sm.tokenize(), equals(expected));
+  group('mandatory parts:', () {
+    test('can tokenize', () {
+      var tag = '0.1.0';
+      var sm = new SemanticVersionTokenizer(tag);
+      var expected = { 'major': 0, 'minor': 1, 'patch': 0 };
+      expect(sm.tokenize(), equals(expected));
+    });
   });
 
-  test('can tokenize tag with alphabetic pre-release part', () {
-    var tag = '0.1.0-alpha';
-    var sm = new SemanticVersionTokenizer(tag);
-    var expected = new Map();
-    expected['major'] = 0;
-    expected['minor'] = 1;
-    expected['patch'] = 0;
-    expected['pre'] = 'alpha';
-    expect(sm.tokenize(), equals(expected));
+  group('pre-release part:', () {
+    test('can tokenize alphabetic', () {
+      var tag = '0.1.0-alpha';
+      var sm = new SemanticVersionTokenizer(tag);
+      var expected = {'major': 0, 'minor': 1, 'patch': 0, 'pre': 'alpha'};
+      expect(sm.tokenize(), equals(expected));
+    });
+
+    test('can tokenize numeric', () {
+      var tag = '0.1.0-100';
+      var sm = new SemanticVersionTokenizer(tag);
+      var expected = {'major': 0, 'minor': 1, 'patch': 0, 'pre': '100'};
+      expect(sm.tokenize(), equals(expected));
+    });
+
+    test('can tokenize alphanumeric', () {
+      var tag = '0.1.0-alpha100';
+      var sm = new SemanticVersionTokenizer(tag);
+      var expected = {'major': 0, 'minor': 1, 'patch': 0, 'pre': 'alpha100'};
+      expect(sm.tokenize(), equals(expected));
+    });
+
+    test('can tokenize complex', () {
+      var tag = '0.1.0-alpha.100';
+      var sm = new SemanticVersionTokenizer(tag);
+      var expected = {'major': 0, 'minor': 1, 'patch': 0, 'pre': 'alpha.100'};
+      expect(sm.tokenize(), equals(expected));
+    });
   });
 
-  test('can tokenize tag with numeric pre-release part', () {
-    var tag = '0.1.0-100';
-    var sm = new SemanticVersionTokenizer(tag);
-    var expected = new Map();
-    expected['major'] = 0;
-    expected['minor'] = 1;
-    expected['patch'] = 0;
-    expected['pre'] = '100';
-    expect(sm.tokenize(), equals(expected));
-  });
+  group('build part:', () {
+    test('can tokenize alphabetic', () {
+      var tag = '0.1.0+alpha';
+      var sm = new SemanticVersionTokenizer(tag);
+      var expected = {'major': 0, 'minor': 1, 'patch': 0, 'build': 'alpha'};
+      expect(sm.tokenize(), equals(expected));
+    });
 
-  test('can tokenize tag with alphanumeric pre-release part', () {
-    var tag = '0.1.0-alpha100';
-    var sm = new SemanticVersionTokenizer(tag);
-    var expected = new Map();
-    expected['major'] = 0;
-    expected['minor'] = 1;
-    expected['patch'] = 0;
-    expected['pre'] = 'alpha100';
-    expect(sm.tokenize(), equals(expected));
-  });
+    test('can tokenize numeric', () {
+      var tag = '0.1.0+100';
+      var sm = new SemanticVersionTokenizer(tag);
+      var expected = {'major': 0, 'minor': 1, 'patch': 0, 'build': '100'};
+      expect(sm.tokenize(), equals(expected));
+    });
 
-  test('can tokenize tag with complex pre-release part', () {
-    var tag = '0.1.0-alpha.100';
-    var sm = new SemanticVersionTokenizer(tag);
-    var expected = new Map();
-    expected['major'] = 0;
-    expected['minor'] = 1;
-    expected['patch'] = 0;
-    expected['pre'] = 'alpha.100';
-    expect(sm.tokenize(), equals(expected));
-  });
+    test('can tokenize alphanumeric', () {
+      var tag = '0.1.0+alpha100';
+      var sm = new SemanticVersionTokenizer(tag);
+      var expected = {'major': 0, 'minor': 1, 'patch': 0, 'build': 'alpha100'};
+      expect(sm.tokenize(), equals(expected));
+    });
 
-  // with build part
-  test('can tokenize tag with alphabetic build part', () {
-    var tag = '0.1.0+alpha';
-    var sm = new SemanticVersionTokenizer(tag);
-    var expected = new Map();
-    expected['major'] = 0;
-    expected['minor'] = 1;
-    expected['patch'] = 0;
-    expected['build'] = 'alpha';
-    expect(sm.tokenize(), equals(expected));
-  });
-
-  test('can tokenize tag with numeric build part', () {
-    var tag = '0.1.0+100';
-    var sm = new SemanticVersionTokenizer(tag);
-    var expected = new Map();
-    expected['major'] = 0;
-    expected['minor'] = 1;
-    expected['patch'] = 0;
-    expected['build'] = '100';
-    expect(sm.tokenize(), equals(expected));
-  });
-
-  test('can tokenize tag with alphanumeric build part', () {
-    var tag = '0.1.0+alpha100';
-    var sm = new SemanticVersionTokenizer(tag);
-    var expected = new Map();
-    expected['major'] = 0;
-    expected['minor'] = 1;
-    expected['patch'] = 0;
-    expected['build'] = 'alpha100';
-    expect(sm.tokenize(), equals(expected));
-  });
-
-  test('can tokenize tag with complex build part', () {
-    var tag = '0.1.0+alpha.100';
-    var sm = new SemanticVersionTokenizer(tag);
-    var expected = new Map();
-    expected['major'] = 0;
-    expected['minor'] = 1;
-    expected['patch'] = 0;
-    expected['build'] = 'alpha.100';
-    expect(sm.tokenize(), equals(expected));
+    test('can tokenize complex', () {
+      var tag = '0.1.0+alpha.100';
+      var sm = new SemanticVersionTokenizer(tag);
+      var expected = {'major': 0, 'minor': 1, 'patch': 0, 'build': 'alpha.100'};
+      expect(sm.tokenize(), equals(expected));
+    });
   });
 }
-
-
